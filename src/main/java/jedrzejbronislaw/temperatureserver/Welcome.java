d9edab03c159d09d51ebeb6d0ddae8aaee8f52d7
JedrzejBronislaw <jedkru@wp.pl>
2021-01-24 00:43:07 +0100

    Add the project

---
+++ src/main/java/jedrzejbronislaw/temperatureserver/Welcome.java
@@ -0,0 +1,13 @@
+package jedrzejbronislaw.temperatureserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Temperature server";
+	}
+}
