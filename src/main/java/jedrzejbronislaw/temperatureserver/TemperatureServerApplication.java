c5c74c8dea1c389edee26a78f1ddc8cf4f858508
JedrzejBronislaw <jedkru@wp.pl>
2021-01-24 01:34:45 +0100

    Adapt the project to Heroku
    Add Spring Boot
    Add build plugin in pom
    Add main class
    Add system.properties file
    Del war packaging in pom
    Del unnecessary dependecies

---
+++ src/main/java/jedrzejbronislaw/temperatureserver/TemperatureServerApplication.java
@@ -0,0 +1,12 @@
+package jedrzejbronislaw.temperatureserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureServerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureServerApplication.class, args);
+	}
+}
